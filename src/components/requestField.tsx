23ce7cc22a85c0c22c533aa3578c360c3a817a65
EmilLykke <emillykkeg@gmail.com>
2022-01-08 12:39:39 +0100

    Added some features
    - Next time i will be looking at making the request with the given varibles

--- src/components/requestField.tsx
+++ src/components/requestField.tsx
@@ -1,7 +1,5 @@
-
 import React, { useRef, useState } from 'react';
-
-
+import axios from 'axios';
 
 interface Props {
     text: string;
@@ -34,21 +32,49 @@ const DivsReducer = (state: State, action: Actions) =>{
     }
 }
 
+function makeUrl(endpoint: string, variable1: string, variable2:string){
+
+  // let total:string = endpoint +"?"+ variable1 + "&" + variable2;
+  let total:string = endpoint ;
+  return total;
+}
+
+function sendRequest(endpoint:string ) {
+  let boom = axios.get("https://api.dev.lunarway.com/mitid/authenticate/parameters");
+  console.log(boom);
+}
+
+
 
 export const RequestField: React.FC<Props> = ({text, handleChange}) =>{
     const [form, setForm] = useState({
         inputValue: ''
-      })
+      });
     
       const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         setForm({ ...form, [event.currentTarget.name]: event.currentTarget.value })
       }
     
+
       const showState = () => {
-        console.log(form)
+        console.log(inputDivs);
+        console.log(variable1.variable1 + " og " + variable2.variable2);
       }
 
+      const [variable1, setVariable1] = useState({
+        variable1: ''
+      });
+      const variable1Handler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setVariable1({ ...variable1, [event.currentTarget.name]: event.currentTarget.value })
+      }
 
+      const [variable2, setVariable2] = useState({
+        variable2: ''
+      })
+      const variable2Handler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setVariable2({ ...variable2, [event.currentTarget.name]: event.currentTarget.value })
+      }
+    
     const inputRef = useRef<HTMLInputElement >(null);
 
     const [inputDivs, dispatch] = React.useReducer(DivsReducer, []);
@@ -58,23 +84,50 @@ export const RequestField: React.FC<Props> = ({text, handleChange}) =>{
           
 
             <input type="text" ref={inputRef} name='inputValue' placeholder={text} onChange={changeHandler}/>
-            <button onClick={showState}>ShowState</button>
+            <button className="btn btn-primary btn-sm distance" onClick={showState}>ShowState</button>
 
-            <button
+            <button className="btn btn-primary btn-sm distance"
             onClick={()=>{
-                dispatch({type: "add", text: form.inputValue})
+                dispatch({type: "add", text: form.inputValue});
             }}
             >
                 +
             </button>
-            <button
+            <button className="btn btn-primary btn-sm distance"
             onClick={()=>{
-                dispatch({type: "remove", idx: inputDivs.length-1})
+                dispatch({type: "remove", idx: inputDivs.length-1});
+                
             }}
             >
                 -
             </button>
-            {JSON.stringify(inputDivs)}
+            <br />
+            <br />
+            <input type="text"  name='variable1' onChange={variable1Handler}/>
+            <br />
+            <br />
+            <input type="text"  name='variable2'  onChange={variable2Handler}/>
+
+            {inputDivs.map(fields =>{
+              return (
+                <div className='field'>
+                  <input type="text" key={fields.id} id={fields.id.toString()} placeholder={makeUrl(fields.text,variable1.variable1, variable2.variable2)}/>
+                  <button className="btn btn-primary btn-sm distance" onClick={()=> sendRequest(makeUrl(fields.text,variable1.variable1, variable2.variable2))}>
+                    Send request
+                  </button>
+                </div>
+              );
+            })}
+            
+            
         </div>
     );
 }
+function setTotalReactPackages(total: any): any {
+  throw new Error('Function not implemented.');
+}
+
+function componentDidMount() {
+  throw new Error('Function not implemented.');
+}
+
